b618d2b54b207ce4c0036f570db7406a182a8a4f
Bernardo Rufino <b.rufino@live.com>
2023-06-01 22:11:46 -0300

    Removed SharePoint auth, using third party library instead.

--- auth.py
+++ auth.py
@@ -1,4 +1,3 @@
-import requests
 from azure.identity import ClientSecretCredential, InteractiveBrowserCredential, TokenCachePersistenceOptions
 
 class Auth:
@@ -16,7 +15,6 @@ class Auth:
         self.tenant_id = tenant_id
         self.client_id = client_id
         self.client_secret = client_secret
-        self.SharePointPrincipal = '00000003-0000-0ff1-ce00-000000000000'
 
 
     def get_token(self) -> str:
@@ -55,45 +53,5 @@ class Auth:
 
         response = auth.get_token(self.scope)
         access_token = response.token
-
-        return access_token
-    
-
-    def get_token_for_sharepoint(self, host_name) -> str:
-        """Creates authenticated SharePoint context via certificate credentials
-
-        """
-        """
-        Generates the bearer token to be used on SharePoint REST API requests.
-
-        Creates authenticated SharePoint context via certificate credentials
-
-        Args:
-
-            host_name (str): host name in the format 'example.sharepoint.com', like 'contoso.sharepoint.com'.
-
-        Returns:
-            str: token for authorization.
-        """
-        resource = f'{self.SharePointPrincipal}/{host_name}@{self.tenant_id}'
-        principal_id = f'{self.client_id}@{self.tenant_id}'
-        url = f'https://accounts.accesscontrol.windows.net/{self.tenant_id}/tokens/OAuth/2'
-
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-
-        data = {
-            'grant_type': 'client_credentials',
-            'client_id': principal_id,
-            'client_secret': self.client_secret,
-            'scope': resource,
-            'resource': resource
-        }
-        
-        response = requests.post(url=url, headers=headers, data=data)
-        response.raise_for_status()
-
-        access_token = response.json().get('access_token', '')
 
         return access_token
