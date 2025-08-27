bc1ad81a27caef5ddbc64c7e7cbcac1d03096bad
Bernardo Rufino <b.rufino@live.com>
2025-08-27 17:57:19 -0300

    Updated to include Fabric authentication.

--- auth.py
+++ auth.py
@@ -11,21 +11,28 @@ class Auth:
             client_id (str, optional): client ID (app registration).
             client_secret (str, optional): client secret/credentials (app registration).
         """
-        self.scope = 'https://analysis.windows.net/powerbi/api/.default'
         self.tenant_id = tenant_id
         self.client_id = client_id
         self.client_secret = client_secret
 
-
-    def get_token(self) -> str:
+    def get_token(self, service: str = 'pbi') -> str:
         """
-        Generates the bearer token to be used on Power BI REST API requests.
+        Generates the bearer token to be used on Power BI or Fabric REST API requests.
 
         Authenticates using a service principal (app account).
+
+        Args:
+            service (str, optional): which service to get token to: pbi or fabric.
 
         Returns:
             str: token for authorization.
         """
+        if service == 'pbi':
+            scope = 'https://analysis.windows.net/powerbi/api/.default'
+        elif service == 'fabric':
+            scope = 'https://api.fabric.microsoft.com/.default'
+        else:
+            raise ValueError("Invalid service specified. Choose 'pbi' or 'fabric'.")
 
         auth = ClientSecretCredential(
                     authority = 'https://login.microsoftonline.com/',
@@ -33,25 +40,34 @@ class Auth:
                     client_id = self.client_id,
                     client_secret = self.client_secret)
 
-        response = auth.get_token(self.scope)
+        response = auth.get_token(scope)
         access_token = response.token
 
         return access_token
 
-
-    def get_token_for_user(self) -> str:
+    def get_token_for_user(self, service: str = 'pbi') -> str:
         """
-        Generates the bearer token to be used on Power BI REST API requests.
+        Generates the bearer token to be used on Power BI or Fabric REST API requests.
 
         Authenticates interactively (user account).
+
+        Args:
+            service (str, optional): which service to get token to: pbi or fabric.
 
         Returns:
             str: token for authorization.
         """
+        if service == 'pbi':
+            scope = 'https://analysis.windows.net/powerbi/api/.default'
+        elif service == 'fabric':
+            scope = 'https://api.fabric.microsoft.com/.default'
+        else:
+            raise ValueError("Invalid service specified. Choose 'pbi' or 'fabric'.")
 
         auth = InteractiveBrowserCredential(cache_persistence_options=TokenCachePersistenceOptions())
 
-        response = auth.get_token(self.scope)
+        response = auth.get_token(scope)
         access_token = response.token
 
         return access_token
+
