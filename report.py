bb8d22419accd0d2a68ca2058bc748b95d64ceeb
Bernardo Rufino <b.rufino@live.com>
2025-10-09 18:43:52 -0300

    Added list report pages and get legacy report JSON definition.

--- report.py
+++ report.py
@@ -14,6 +14,7 @@ class Report:
         Initialize variables.
         """
         self.main_url = 'https://api.powerbi.com/v1.0/myorg'
+        self.main_fabric_url = 'https://api.fabric.microsoft.com/v1'
         self.token = token
         self.headers = {'Authorization': f'Bearer {self.token}'}
         self.data_dir = './data/reports'
@@ -28,7 +29,7 @@ class Report:
         List all reports on a specific workspace_id that the user has access to.
 
         Args:
-            workspace_id (str, optional): workspace id to search datasets from.
+            workspace_id (str, optional): workspace id to search reports from.
 
         Returns:
             Dict: status message and content.
@@ -68,6 +69,106 @@ class Report:
                 return {'message': {'error': error_message, 'content': response}}
 
 
+    def list_report_pages(
+                self,
+                workspace_id: str = '',
+                report_id: str = '') -> Dict:
+        """
+        List all report pages on a specific report_id and workspace_id that the user has access to.
+
+        Args:
+            workspace_id (str, optional): workspace id where the report is.
+            report_id (str, optional): report id to search pages from.
+
+        Returns:
+            Dict: status message and content.
+        """
+
+        # Main URL
+        request_url = f'{self.main_url}/groups/{workspace_id}/reports/{report_id}/pages'
+
+        # If workspace ID was not informed, return error message...
+        if workspace_id == '':
+            return {'message': 'Missing workspace id, please check.', 'content': ''}
+
+        # If workspace ID was informed...
+        else: 
+            filename = f'report_pages_{report_id}.xlsx'
+            filepath = f'{self.data_dir}/pages/{filename}'
+            os.makedirs(filepath, exist_ok=True)
+
+            # Make the request
+            r = requests.get(url=request_url, headers=self.headers)
+
+            # Get HTTP status and content
+            status = r.status_code
+            response = json.loads(r.content).get('value', '')
+
+            # If success...
+            if status == 200:
+                # Save to Excel file
+                df = pd.DataFrame(response)
+                try:
+                    df.to_excel(filepath, index=False)
+                except PermissionError as error:
+                    print('File is open already, cannot save it. Skipping...')
+
+                return {'message': 'Success', 'content': response}
+
+            else:                
+                # If any error happens, return message.
+                response = json.loads(r.content)
+                error_message = response['error']['message']
+
+                return {'message': {'error': error_message, 'content': response}}
+
+
+    def get_report_definition(
+                self,
+                workspace_id: str = '',
+                report_id: str = '') -> Dict:
+        """
+        List all report pages on a specific report_id and workspace_id that the user has access to.
+
+        Args:
+            workspace_id (str, optional): workspace id where the report is.
+            report_id (str, optional): report id to search pages from.
+
+        Returns:
+            Dict: status message and content.
+        """
+
+        # Main URL
+        request_url = f'{self.main_fabric_url}/workspaces/{workspace_id}/reports/{report_id}/getDefinition'
+
+        # If workspace ID or report ID was not informed, return error message...
+        if workspace_id == '':
+            return {'message': 'Missing workspace id, please check.', 'content': ''}
+        if report_id == '':
+            return {'message': 'Missing report id, please check.', 'content': ''}
+
+        # Continue...
+        else:
+
+            # Make the request
+            r = requests.post(url=request_url, headers=self.headers)
+
+            # Get HTTP status and content
+            status = r.status_code
+            response = json.loads(r.content)
+
+            # If success...
+            if status == 202:
+                operation_id = r.headers.get('x-ms-operation-id')
+                return {'message': 'Success', 'content': {'workspace_id': workspace_id, 'report_id': report_id, 'operation_id': operation_id } }
+
+            else:                
+                # If any error happens, return message.
+                error_message = response['error']['message']
+
+                return {'message': {'error': error_message, 'content': response}}
+
+
     def export_report(
                 self, 
                 workspace_id: str = '',
@@ -90,6 +191,15 @@ class Report:
         Returns:
             Dict: status message and content.
         """
+        filename = f'{report_name}.pbix'
+        file_path = f'{self.data_dir}/exports/{dataset_name}/{workspace_name}'
+
+        file_exists = os.path.exists(f'{file_path}/{filename}')
+
+        if (not replace_existing) and (file_exists):
+            return {'message': f'File {filename} already exists.', 'content': ''}
+        
+        print(f'Exporting {report_name}')
 
         # Main URL
         request_url = f'{self.main_url}/groups/{workspace_id}/reports/{report_id}/Export/?DownloadType=LiveConnect'
@@ -102,8 +212,6 @@ class Report:
 
         # If workspace ID and report ID were informed...
         else: 
-            filename = f'{report_name}.pbix'
-            file_path = f'{self.data_dir}/exports/{workspace_name}/{dataset_name}'
 
             create_directory(file_path)
 
@@ -115,15 +223,13 @@ class Report:
 
             # If success...
             if status == 200:
-                if not replace_existing and os.path.exists(f'{file_path}/{filename}'):
-                    return {'message': f'File {filename} already exists.', 'content': ''}
                 # Save to PBIX file
-                else:
-                    with open(f'{file_path}/{filename}', 'wb') as f:
-                        f.write(r.content)
+                with open(f'{file_path}/{filename}', 'wb') as f:
+                    f.write(r.content)
                 
                 return {'message': 'Success', 'content': 'File downloaded successfully.'}
 
-            else:                
+            else:
+                print(f'Error exporting {report_name}:\n{r.content}')
 
-                return {'message': {'error': f'Error with status code {status}', 'content': ''}}
+                return {'message': {'error': f'Error with status code {status}'}, 'content': ''}
